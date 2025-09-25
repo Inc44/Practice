8293bcca58ecb74eb2bfc92a569ea5a47f5f7936
Inc <121856799+Inc44@users.noreply.github.com>
2025-09-25 09:23:49 +0200

    java tp1 partie4

--- L2/Java/tp1ProgrammationJavaSyntaxeDeBase/partie4Tableaux/tableauA2Dimensions/Exercice36RechercheDePointsCols.java
+++ L2/Java/tp1ProgrammationJavaSyntaxeDeBase/partie4Tableaux/tableauA2Dimensions/Exercice36RechercheDePointsCols.java
@@ -1,3 +1,57 @@
 package tp1ProgrammationJavaSyntaxeDeBase.partie4Tableaux.tableauA2Dimensions;
 
-public class Exercice36RechercheDePointsCols {}
+import java.util.Scanner;
+
+public class Exercice36RechercheDePointsCols {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entrez un nombre de lignes : ");
+		int rows = sc.nextInt();
+		System.out.print("Entrez un nombre de colonnes : ");
+		int cols = sc.nextInt();
+		int[][] mat = new int[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			System.out.print("Entrez les elements de la ligne " + i + " : ");
+			for (int j = 0; j < cols; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		boolean[][] max = new boolean[rows][cols];
+		boolean[][] min = new boolean[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			int rowMax = mat[i][0];
+			for (int j = 0; j < cols; j++) {
+				if (mat[i][j] > rowMax) {
+					rowMax = mat[i][j];
+				}
+			}
+			for (int j = 0; j < cols; j++) {
+				if (mat[i][j] == rowMax) {
+					max[i][j] = true;
+				}
+			}
+		}
+		for (int j = 0; j < cols; j++) {
+			int colMin = mat[0][j];
+			for (int i = 0; i < rows; i++) {
+				if (mat[i][j] < colMin) {
+					colMin = mat[i][j];
+				}
+			}
+			for (int i = 0; i < rows; i++) {
+				if (mat[i][j] == colMin) {
+					min[i][j] = true;
+				}
+			}
+		}
+		System.out.println("Points-cols :");
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (max[i][j] && min[i][j]) {
+					System.out.println("mat[" + i + "][" + j + "] = " + mat[i][j]);
+				}
+			}
+		}
+		sc.close();
+	}
+}
--- L2/Java/tp1ProgrammationJavaSyntaxeDeBase/partie4Tableaux/tableauA2Dimensions/Exercice37RechercheDeSousMatrice.java
+++ L2/Java/tp1ProgrammationJavaSyntaxeDeBase/partie4Tableaux/tableauA2Dimensions/Exercice37RechercheDeSousMatrice.java
@@ -1,3 +1,59 @@
 package tp1ProgrammationJavaSyntaxeDeBase.partie4Tableaux.tableauA2Dimensions;
 
-public class Exercice37RechercheDeSousMatrice {}
+import java.util.Scanner;
+
+public class Exercice37RechercheDeSousMatrice {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entrez un nombre de lignes de la matrice principale : ");
+		int rows = sc.nextInt();
+		System.out.print("Entrez un nombre de colonnes de la matrice principale : ");
+		int cols = sc.nextInt();
+		int matrix[][] = new int[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			System.out.print("Entrez les elements de la ligne " + i + " : ");
+			for (int j = 0; j < cols; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		/*
+		int rows = 12;
+		int cols = 12;
+		int matrix[][] = {{0, 0, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1},
+			{2, 1, 1, 0, 1, 0, 0, 1, 2, 2, 0, 1}, {2, 2, 1, 0, 1, 1, 2, 0, 0, 2, 1, 2},
+			{2, 0, 0, 2, 2, 0, 1, 0, 2, 1, 0, 1}, {1, 2, 2, 2, 1, 0, 1, 1, 2, 2, 0, 2},
+			{0, 1, 2, 2, 0, 1, 1, 1, 1, 2, 1, 1}, {0, 0, 2, 0, 2, 2, 1, 2, 0, 0, 0, 0},
+			{0, 0, 1, 0, 2, 0, 2, 2, 0, 2, 0, 2}, {2, 2, 2, 0, 0, 1, 1, 2, 0, 1, 1, 1},
+			{1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 1}, {2, 0, 1, 2, 1, 0, 0, 1, 2, 2, 2, 2},
+			{1, 0, 1, 0, 0, 0, 0, 1, 0, 2, 1, 1}};
+		*/
+		System.out.print("Entrez un nombre de lignes de la sous-matrice : ");
+		int subRows = sc.nextInt();
+		System.out.print("Entrez un nombre de colonnes de la sous-matrice : ");
+		int subCols = sc.nextInt();
+		int subMatrix[][] = new int[subRows][subCols];
+		for (int i = 0; i < subRows; i++) {
+			System.out.print("Entrez les elements de la ligne " + i + " : ");
+			for (int j = 0; j < subCols; j++) {
+				subMatrix[i][j] = sc.nextInt();
+			}
+		}
+		for (int i = 0; i < rows - subRows; i++) {
+			for (int j = 0; j < cols - subCols; j++) {
+				boolean match = true;
+				for (int subI = 0; subI < subRows && match; subI++) {
+					for (int subJ = 0; subJ < subCols && match; subJ++) {
+						if (matrix[i + subI][j + subJ] != subMatrix[subI][subJ]) {
+							match = false;
+						}
+					}
+				}
+				if (match) {
+					System.out.println("Position de la sous-matrice : " + i + " " + j);
+				}
+			}
+		}
+
+		sc.close();
+	}
+}
