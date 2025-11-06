5537e7d42e658b63870b0f9df82b479ab31ec772
Inc44 <incognita44@outlook.com>
2025-11-06 17:43:13 +0100

    sql tp1LDDv3 LMD

--- L2/SQL/tp1LDDv3/tp1LDDv3.sql
+++ L2/SQL/tp1LDDv3/tp1LDDv3.sql
@@ -132,6 +132,7 @@ ALTER TABLE
 	analyseeau
 ADD
 	CONSTRAINT chk_analyseEau_reservoirId CHECK (reservoirId IS NOT NULL);
+-- Fails for MariaDB 10.11.14
 -- 7e
 ALTER TABLE
 	analyseeau
@@ -165,6 +166,7 @@ WHERE
 		captageId IS NULL
 		AND reservoirId IS NULL
 	);
+-- Fails for MariaDB 10.11.14
 -- 8
 CREATE TABLE releve (
 	analyseEauId INT NOT NULL,
@@ -178,3 +180,180 @@ CREATE TABLE releve (
 -- 9
 DROP TABLE reservoir;
 -- Cannot delete or update a parent row: a foreign key constraint fails due to `reservoir` using `fk_analyseEau_reservoirId` in table `analyseeau`
+-- 10
+SELECT
+	DISTINCT c.nom
+FROM
+	captage c
+	INNER JOIN analyseeau a ON c.captageId = a.captageId
+WHERE
+	a.dateAnalyse BETWEEN '2018-01-01' AND '2018-12-31'
+	AND c.nom LIKE '%AAC%'
+ORDER BY
+	c.nom;
+-- 11
+SELECT
+	DISTINCT r.reservoirId,
+	r.capaciteMax
+FROM
+	reservoir r
+	INNER JOIN analyseeau a ON r.reservoirId = a.reservoirId
+	INNER JOIN releve re ON a.analyseEauId = re.analyseEauId
+	INNER JOIN substance s ON re.substanceId = s.substanceId
+WHERE
+	r.typeReservoir = 'enterré'
+	AND r.capaciteMax > 15000
+	AND s.libelle = 'Nitrates'
+	AND re.qteReleve > 20;
+-- 12
+SELECT
+	DISTINCT c.nom
+FROM
+	captage c
+	INNER JOIN analyseeau a ON c.captageId = a.captageId
+	INNER JOIN releve re ON a.analyseEauId = re.analyseEauId
+	INNER JOIN substance s ON re.substanceId = s.substanceId
+WHERE
+	c.nom LIKE 'AAC%'
+	AND s.libelle = 'Atrazine'
+	AND re.qteReleve < 0.065
+	AND a.dateAnalyse BETWEEN '2019-01-01' AND '2019-12-31';
+-- 13
+SELECT
+	DISTINCT t.nom,
+	t.prenom,
+	a.dateAnalyse,
+	r.nom
+FROM
+	technicien t
+	INNER JOIN analyseeau a ON t.technicienId = a.technicienId
+	INNER JOIN reservoir r ON a.reservoirId = r.reservoirId
+WHERE
+	a.reservoirId IN (
+		SELECT
+			a.reservoirId
+		FROM
+			analyseeau a
+			INNER JOIN technicien t ON a.technicienId = t.technicienId
+		WHERE
+			t.nom = 'LABAT'
+			AND t.prenom = 'ALEXANDRA'
+	)
+	AND NOT (
+		t.nom = 'LABAT'
+		AND t.prenom = 'ALEXANDRA'
+	);
+-- 14
+SELECT
+	c.nom,
+	COUNT(a.analyseEauId)
+FROM
+	captage c
+	INNER JOIN analyseeau a ON c.captageId = a.captageId
+GROUP BY
+	c.nom
+HAVING
+	COUNT(a.analyseEauId) < 3;
+-- 15
+SELECT
+	libelle
+FROM
+	substance
+WHERE
+	substanceId NOT IN (
+		SELECT
+			DISTINCT substanceId
+		FROM
+			releve
+	);
+SELECT
+	s.libelle
+FROM
+	substance s
+	LEFT JOIN releve r ON s.substanceId = r.substanceId
+WHERE
+	r.analyseEauId IS NULL;
+-- 6 records instead of 4
+-- 16
+SELECT
+	s.libelle,
+	r.qteReleve,
+	s.qteMax,
+	a.dateAnalyse
+FROM
+	substance s
+	LEFT JOIN releve r ON s.substanceId = r.substanceId
+	LEFT JOIN analyseeau a ON r.analyseEauId = a.analyseEauId
+ORDER BY
+	s.libelle;
+-- 17
+SELECT
+	MONTH(a.dateAnalyse),
+	AVG(r.qteReleve)
+FROM
+	releve r
+	INNER JOIN substance s ON r.substanceId = s.substanceId
+	INNER JOIN analyseeau a ON r.analyseEauId = a.analyseEauId
+	INNER JOIN reservoir re ON a.reservoirId = re.reservoirId
+WHERE
+	s.libelle = 'Nitrates'
+	AND a.dateAnalyse BETWEEN '2019-01-01' AND '2019-12-31'
+	AND re.typeReservoir = 'enterré'
+	AND re.capaciteMax < 20000
+GROUP BY
+	MONTH(a.dateAnalyse)
+ORDER BY
+	1;
+-- 20000 instead of 10000
+-- 18
+SELECT
+	t.nom,
+	t.prenom,
+	COUNT(*)
+FROM
+	technicien t
+	INNER JOIN analyseeau a ON t.technicienId = a.technicienId
+GROUP BY
+	t.nom,
+	t.prenom
+HAVING
+	COUNT(*) >= ALL (
+		SELECT
+			COUNT(*)
+		FROM
+			analyseeau
+		GROUP BY
+			technicienId
+	);
+-- 19
+SELECT
+	s.libelle,
+	COUNT(r.analyseEauId)
+FROM
+	substance s
+	INNER JOIN releve r ON s.substanceId = r.substanceId
+GROUP BY
+	s.libelle
+HAVING
+	COUNT(r.analyseEauId) <= ALL (
+		SELECT
+			COUNT(r.analyseEauId)
+		FROM
+			substance s
+			INNER JOIN releve r ON s.substanceId = r.substanceId
+		GROUP BY
+			s.substanceId
+	);
+-- 20
+SELECT
+	r.nom,
+	COUNT(DISTINCT a.technicienId)
+FROM
+	reservoir r
+	INNER JOIN analyseeau a ON r.reservoirId = a.reservoirId
+WHERE
+	r.typeReservoir = 'enterré'
+	AND a.dateAnalyse BETWEEN '2018-07-01' AND '2018-12-31'
+GROUP BY
+	r.reservoirId,
+	r.nom;
