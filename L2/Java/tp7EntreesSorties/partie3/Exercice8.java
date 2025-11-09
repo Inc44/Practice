45cc4238ccfa186e5feabbd61b1f1247c93bdfd5
Inc <121856799+Inc44@users.noreply.github.com>
2025-11-09 19:34:36 +0100

    java tp7EntreesSorties partie3 binaire direct

--- L2/Java/tp7EntreesSorties/partie3/Exercice8.java
+++ L2/Java/tp7EntreesSorties/partie3/Exercice8.java
@@ -2,50 +2,42 @@ package tp7EntreesSorties.partie3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.RandomAccessFile;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Exercice8 {
 	public static void createFile(String fileName, int count, int min, int max) {
-		try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+		try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
 			Random rand = new Random();
 			for (int i = 0; i < count; i++) {
 				int number = rand.nextInt(max - min) + min;
-				writer.println(number);
+				raf.writeInt(number);
 			}
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
 	}
 	public static void readFile(String fileName) {
-		try (Scanner sc = new Scanner(new File(fileName))) {
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		try (RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+			while (raf.getFilePointer() < raf.length()) {
+				System.out.println(raf.readInt());
 			}
 		} catch (FileNotFoundException exception) {
 			System.err.println("File not found: " + exception.getMessage());
+		} catch (IOException exception) {
+			exception.printStackTrace();
 		}
 	}
 	public static void addFive(String fileName) {
-		List<Integer> numbers = new ArrayList<>();
-		try (Scanner sc = new Scanner(new File(fileName))) {
-			while (sc.hasNextInt()) {
-				int number = sc.nextInt();
-				numbers.add(number + 5);
+		try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
+			while (raf.getFilePointer() < raf.length()) {
+				int number = raf.readInt();
+				raf.seek(raf.getFilePointer() - 4);
+				raf.writeInt(number + 5);
 			}
 		} catch (FileNotFoundException exception) {
 			System.err.println("File not found: " + exception.getMessage());
-			return;
-		}
-		try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
-			for (int number : numbers) {
-				writer.println(number);
-			}
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
@@ -55,7 +47,7 @@ public class Exercice8 {
 		if (!dataDir.exists()) {
 			dataDir.mkdir();
 		}
-		String fileName = "data/.txt";
+		String fileName = "data/.dat";
 		createFile(fileName, 15, -50, 50);
 		readFile(fileName);
 		addFive(fileName);
--- L2/Java/tp7EntreesSorties/partie3/Exercice9.java
+++ L2/Java/tp7EntreesSorties/partie3/Exercice9.java
@@ -2,50 +2,44 @@ package tp7EntreesSorties.partie3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.RandomAccessFile;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Exercice9 {
 	public static void createFile(String fileName, int count, double min, double max) {
-		try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+		try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
 			Random rand = new Random();
 			for (int i = 0; i < count; i++) {
 				double number = rand.nextDouble(max - min) + min;
-				writer.println(number);
+				raf.writeDouble(number);
 			}
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
 	}
 	public static void readFile(String fileName) {
-		try (Scanner sc = new Scanner(new File(fileName))) {
-			while (sc.hasNextDouble()) {
-				System.out.println(sc.nextDouble());
+		try (RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+			while (raf.getFilePointer() < raf.length()) {
+				System.out.println(raf.readDouble());
 			}
 		} catch (FileNotFoundException exception) {
 			System.err.println("File not found: " + exception.getMessage());
+		} catch (IOException exception) {
+			exception.printStackTrace();
 		}
 	}
 	public static void zeroNegatives(String fileName) {
-		List<Double> numbers = new ArrayList<>();
-		try (Scanner sc = new Scanner(new File(fileName))) {
-			while (sc.hasNextDouble()) {
-				double number = sc.nextDouble();
-				numbers.add(number < 0 ? 0 : number);
+		try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
+			while (raf.getFilePointer() < raf.length()) {
+				double number = raf.readDouble();
+				if (number < 0) {
+					raf.seek(raf.getFilePointer() - 8);
+					raf.writeDouble(0.0);
+				}
 			}
 		} catch (FileNotFoundException exception) {
 			System.err.println("File not found: " + exception.getMessage());
-			return;
-		}
-		try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
-			for (double number : numbers) {
-				writer.println(number);
-			}
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
@@ -55,7 +49,7 @@ public class Exercice9 {
 		if (!dataDir.exists()) {
 			dataDir.mkdir();
 		}
-		String fileName = "data/.txt";
+		String fileName = "data/.dat";
 		createFile(fileName, 10, -50.0, 50.0);
 		readFile(fileName);
 		zeroNegatives(fileName);
