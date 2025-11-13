5bf37725562e6766eee89594df74b7fd9fce8574
Inc <121856799+Inc44@users.noreply.github.com>
2025-11-13 04:29:30 +0100

    javascript tp120252026 purecounter, space, header, media

--- L2/JavaScript/tp120252026/script.js
+++ L2/JavaScript/tp120252026/script.js
@@ -1,1 +1,13 @@
-new PureCounter();
+(function()
+{
+	var width = window.innerWidth / 16;
+	var list = [70, 62, 33, 16];
+	if (width <= 90) list = [44, 38, 20, 10];
+	if (width <= 30) list = [95, 84, 45, 22];
+	var purecounters = document.querySelectorAll(".purecounter");
+	for (var i = 0; i < purecounters.length; i++)
+	{
+		purecounters[i].setAttribute("data-purecounter-end", list[i]);
+	}
+	new PureCounter();
+})();
