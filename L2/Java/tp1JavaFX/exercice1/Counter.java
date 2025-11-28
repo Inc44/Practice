26e5be4fc74630f17e6ad157d2d29d4412fe3442
Inc <121856799+Inc44@users.noreply.github.com>
2025-11-28 09:15:16 +0100

    java tp1JavaFX exercice1

---
+++ L2/Java/tp1JavaFX/exercice1/Counter.java
@@ -0,0 +1,14 @@
+package tp1JavaFX.exercice1;
+
+public class Counter {
+	private int i = 0;
+	public int getValue() {
+		return i;
+	}
+	public int dec() {
+		return --i;
+	}
+	public int inc() {
+		return ++i;
+	}
+}
---
+++ L2/Java/tp1JavaFX/exercice1/Main.java
@@ -0,0 +1,42 @@
+package tp1JavaFX.exercice1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*
+ *
+ * Exemple JavaFX et FXML
+ *
+ *
+ * Avec un JDK version 11 ou +,
+ * la JVM doit être lancée sous Windows avec les options suivantes,
+ * en adaptant le chemin vers les librairies JavaFX,
+ *
+ * sur Windows :
+ *   --module-path "C:\Program Files\javafx-sdk\lib" --add-modules javafx.controls,javafx.fxml
+ *
+ * sur Mac et linux :
+ *   --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml
+ *
+ */
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Counter counter = new Counter();
+
+		FXMLLoader mainWindowLoader = new FXMLLoader(getClass().getResource("mainWindow.fxml"));
+		mainWindowLoader.setController(new MainWindow(counter));
+
+		Scene scene = new Scene(mainWindowLoader.load());
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
